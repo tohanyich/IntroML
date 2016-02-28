94dc6d6fb8d00f97d6d905e1a8c953fac48055eb
Anton Marchenkov <marchenkov.anton@gmail.com>
2016-02-28 23:30:03 +0300

    Доделал до конца. Переделал восстановление и сравнение изображений

--- Week6/clustering.py
+++ Week6/clustering.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 from sklearn.cluster import KMeans
 import skimage.io as io
 from skimage.color import rgb2gray
@@ -12,26 +13,19 @@ def recreate_image(codebook, labels, w, h):
     d = codebook.shape[1]
     image = np.zeros((w, h, d))
     label_idx = 0
-    print(codebook)
-    print(labels)
+ #   print(codebook)
+ #   print(labels)
     for i in range(w):
         for j in range(h):
             image[i][j] = codebook[labels[label_idx]]
             label_idx += 1
     return image
 
-def MSE(orig_image, ch_image):
-    w, h = orig_image.shape
-    M = orig_image - ch_image
-    mse = np.sum(M*M)/(w*h)
-#    print('mse = %f') % mse
-    return mse 
-
-n_colors = 2
+n_colors = 11
 
 image = io.imread('parrots.jpg')
 w, h, d = original_shape = tuple(image.shape)
-print('w = %d h = %d d = %d') %(w,h,d)
+#print('w = %d h = %d d = %d') %(w,h,d)
 
 gray_im = rgb2gray(image)
 fl_im = img_as_float(image)
@@ -40,8 +34,24 @@ image_array = np.reshape(fl_im, (w * h, d))
 kmeans = KMeans(n_clusters=n_colors, init='k-means++', random_state=241).fit(image_array)
 labels = kmeans.predict(image_array)
 
-new_im = recreate_image(kmeans.cluster_centers_, labels, w, h)
-PSNR = 10*np.log10(1./MSE(rgb2gray(fl_im),rgb2gray(new_im)))
+#new_im = recreate_image(kmeans.cluster_centers_, labels, w, h)
+new_im = fl_im
+r = np.array([new_im[:,:,0].ravel()]).T
+g = np.array([new_im[:,:,1].ravel()]).T
+b = np.array([new_im[:,:,2].ravel()]).T
+#labels = labels.reshape((w,h))
+for cluster in range(0, n_colors):
+    mr = np.mean(r[labels == cluster])
+    r[labels == cluster] = mr
+    mg = np.mean(g[labels == cluster])
+    g[labels == cluster] = mg
+    mb = np.mean(b[labels == cluster])
+    b[labels == cluster] = mb
+    new_im = np.hstack((r,g))
+    new_im = np.hstack((new_im,b))
+new_im = np.reshape(new_im,(w,h,d))
+
+PSNR = 10*np.log10(1./mean_squared_error(image_array,np.reshape(new_im,(w*h,d))))
 
 # Display all results, alongside original image
 plt.figure(1)
@@ -56,6 +66,6 @@ plt.clf()
 ax = plt.axes([0, 0, 1, 1])
 plt.axis('off')
 plt.title('Quantized image (Clusters = %d, PSNR = %f)'%(n_colors,PSNR)) 
-plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
+plt.imshow(new_im)
 
 plt.show()
