52640832ef7ccc345ce9fd2ccd3704663620e7e1
Anton Marchenkov <marchenkov.anton@gmail.com>
2016-03-03 23:59:36 +0300

    Начал делать итоговое задание

---
+++ Week7/final_task.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+import pandas as pd
+features = pd.read_csv('./features.csv', index_col='match_id')
+target = features[u'radiant_win']
+
+del features[u'duration'],features[u'radiant_win'], features[u'tower_status_radiant']
+del features[u'tower_status_dire'],features[u'barracks_status_radiant'], features[u'barracks_status_dire']
+
+str_num,col_num = features.shape
+col_names = features.columns
+
+#Выведем столбцы с пропусками и обработаем их
+for name in col_names:
+    num = features[name].count()
+    if num < str_num:
+        print('%d Null elements in column %s') %(str_num - num, name)
+        features[name].fillna(value = 0)
